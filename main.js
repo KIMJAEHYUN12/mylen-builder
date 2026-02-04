0acef7b23bd6bd2916a41a2a03c7dfe23e3743b1
jeusjeahyun <jeusjeahyun@gmail.com>
2026-02-04 02:01:41 +0000

    feat: Add dark/light mode functionality

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 const numbersDisplay = document.querySelector('.numbers-display');
 const generateBtn = document.querySelector('.generate-btn');
+const themeToggleButton = document.querySelector('.theme-toggle-btn');
+
+// Theme logic
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.body.classList.add(currentTheme);
+} else {
+    document.body.classList.add('light-mode'); // Default theme
+    localStorage.setItem('theme', 'light-mode');
+}
+
+themeToggleButton.addEventListener('click', () => {
+    if (document.body.classList.contains('light-mode')) {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+        localStorage.setItem('theme', 'light-mode');
+    }
+});
+
+
 
 const generateNumbers = () => {
     const numbers = new Set();
